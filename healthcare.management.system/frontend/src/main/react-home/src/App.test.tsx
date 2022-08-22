25f96ecca029367560e6da8277471e874504668b
Nedislav Kamburov -X (nkamburo - SOFTSERVE INC at Cisco) <nkamburo@cisco.com>
2022-08-22 12:04:56 +0300

    folder restucturing

--- healthcare.management.system/frontend/src/main/react-home/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the React App header', () => {
-  render(<App />);
-  const appElement = screen.getByRole(/heading/i);
-  expect(appElement).toBeInTheDocument();
-});
---
+++ mvc-module/src/main/java/com/healthcare/management/system/mvc/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.healthcare.management.system.mvc.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.healthcare.management.system.mvc.controller")
+public class WebConfig {
+    // All web configuration will go here
+}
